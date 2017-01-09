832783c48b96bf64f24fcfbbae5d726dc550f7cb
Kevin Jing Qiu <kevin@idempotent.ca>
2017-01-09 00:31:46 -0500

    Implemented existing games endpoint

--- goal/app/existing_game.py
+++ goal/app/existing_game.py
@@ -1,12 +1,12 @@
+import os
 from .base import v2_route, app
 
 
 @v2_route(app, '/v2/games')
 def get_existing_games():
+    games = [{'id': f.split('.db')[0]} for f in os.listdir('.') if f.endswith('.db')]
     return {
-        'games': [
-            {'id': 'abcdef'}  # TODO
-        ]
+        'games': games
     }
 
 
